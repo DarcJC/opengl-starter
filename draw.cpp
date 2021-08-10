e9870ea7e4771effb04d1c6ef06e651d23c6390f
DarcJC <me@darc.pro>
2021-08-10 10:47:02 +0800

    清理分支

--- draw.cpp
+++ draw.cpp
@@ -33,8 +33,6 @@ void Draw::drawCall() const {
     shaderProgram.setInt("texture1", 1);
     glBindVertexArray(vao);
     glDrawArrays(GL_TRIANGLES, 0, 40);
-//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-//    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
     glBindVertexArray(0); // 重置VAO 以免被其他渲染操作污染
 }
 
@@ -50,17 +48,11 @@ void Draw::initBuffer() {
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     // 复制顶点和索引到缓冲区中
     glBufferData(GL_ARRAY_BUFFER, sizeof(boxVertices), boxVertices, GL_STATIC_DRAW);
-//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-//    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     // 设置顶点属性指针
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)nullptr); // 位置属性
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-//    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))); // 颜色属性
-//    glEnableVertexAttribArray(1);
-//    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float))); // 纹理坐标
-//    glEnableVertexAttribArray(2);
     // 取消绑定VAO
     glBindVertexArray(0);
 }
